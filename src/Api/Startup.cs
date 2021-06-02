20b5e4823de31f11b0a268234d77500a07abaf81
PBonvang <peter.bonvang@gmail.com>
2021-06-02 10:17:19 +0200

    Uncaught exception handler added to startup;

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -54,9 +54,10 @@ namespace Api
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
+            else
+                app.UseExceptionHandler("/error");
+            
 
             app.UseHttpsRedirection();
 
